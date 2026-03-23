0f9ad50ed7d827e644510f5843b96e5c6f25c05c
stefvandebergh <stef.vandebergh@telenet.be>
2026-03-23 21:37:13 +0100

    2: add player and asteroid objects

---
+++ asteroid.py
@@ -0,0 +1,1 @@
+from circleshape import CircleShape
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,13 +9,29 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        updatable.update(dt)
+        for element in drawable:
+            element.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60)/1000
         
 
 if __name__ == "__main__":
