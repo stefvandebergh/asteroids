00781085fb1756b350c01875b62679a2d3ad6bfb
stefvandebergh <stef.vandebergh@telenet.be>
2026-03-24 11:02:43 +0100

    6: add irregular shapes for asteroids and test.py file

--- asteroid.py
+++ asteroid.py
@@ -1,15 +1,16 @@
 import pygame
 import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH,ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
+from constants import LINE_WIDTH,ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_RADIUS_INTERVAL, ANGLES
 from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.point_diff_list = [self.radius + random.uniform( -ASTEROID_RADIUS_INTERVAL, ASTEROID_RADIUS_INTERVAL) for angle in ANGLES]
     
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        pygame.draw.polygon(screen, "white", self.asteroid_shape(), LINE_WIDTH)
     
     def update(self, dt):
         self.position += self.velocity*dt
@@ -26,4 +27,11 @@ class Asteroid(CircleShape):
         asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid1.velocity = self.velocity.rotate(angle) *1.2
         asteroid2.velocity = self.velocity.rotate(-angle)*1.2
-        
+    
+    def asteroid_shape(self):
+        pointlist = []
+        for angle_i in range(len(ANGLES)):
+            diff = self.point_diff_list[angle_i]
+            point = self.position + pygame.Vector2(0,1).rotate(ANGLES[angle_i])*diff
+            pointlist.append(point)
+        return pointlist
